1237cd0a0b6e4591b110ee7c437d30f066637720
Emil324234 <145430552+Emil324234@users.noreply.github.com>
2024-03-19 00:33:38 +0300

    Add files via upload

---
+++ MainWindowlaba3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using prct3Lab3.DS3DataSetTableAdapters;
+using System.Windows;
+
+namespace prct3Lab3
+{
+    public partial class MainWindow : Window
+    {
+        private PlayersTableAdapter players = new PlayersTableAdapter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeaponGRD.ItemsSource = players.GetPlayers();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WeaponGRD.Columns[0].Visibility = Visibility.Collapsed;
+            WeaponGRD.Columns[2].Visibility = Visibility.Collapsed;
+        }
+    }
+}
